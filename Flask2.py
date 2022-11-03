841600b44e72a598956b7eba69fa53505a4782bc
Emerson Vitor Pereira da Silva <63209398+Emerson-Vitor@users.noreply.github.com>
2022-11-03 17:08:44 -0300

    Create Flask2.py

---
+++ Flask2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    name = request.args.get("name")
+    return render_template('index.html',name=name)
+
+@app.route("/sobre")
+def sobre():
+    return render_template('sobre.html')
+
+@app.route("/recursos")
+def recursos():
+    return render_template('recursos.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
